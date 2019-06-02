cb2fa39af98048bfdaec463ce1302d3cdb964f91
DAO <46238164+mrdaovn@users.noreply.github.com>
2019-06-02 21:54:35 +0700

    Create lenhdieukhienswitch.go

---
+++ lenhdieukhienswitch.go
@@ -0,0 +1,10 @@
+
+switch i {
+case 0:  fmt.Println("Khong")
+case 1:  fmt.Println("Mot")
+case 2:  fmt.Println("Hai")
+case 3:  fmt.Println("Ba")
+case 4:  fmt.Println("Bon")
+case 5:  fmt.Println("Nam")
+default: fmt.Println("Khong biet")
+}
